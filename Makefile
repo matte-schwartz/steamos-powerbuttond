5ccfdceb1f5464108e2c681cf0c1e4ab5e3f2825
Vicki Pfau <vi@endrift.com>
2025-02-19 18:59:34 -0800

    Autodetect power button instead of specifying manually

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-CFLAGS := $(shell pkg-config --cflags libevdev) -Wall -Wextra -O2
-LDFLAGS := $(shell pkg-config --libs libevdev) -O2
+CFLAGS += $(shell pkg-config --cflags libevdev libudev) -Wall -Wextra -O2
+LDFLAGS += $(shell pkg-config --libs libevdev libudev) -O2
 
 all: powerbuttond
 .PHONY: clean install
@@ -16,6 +16,6 @@ clean:
 install: all LICENSE
 	install -Ds -m 755 powerbuttond $(DESTDIR)/usr/lib/hwsupport/powerbuttond
 	install -D -m 644 LICENSE $(DESTDIR)/usr/share/licenses/powerbuttond
-	install -D -m 644 powerbuttond@.service $(DESTDIR)/usr/lib/systemd/user/powerbuttond@.service
+	install -D -m 644 steamos-powerbuttond.service $(DESTDIR)/usr/lib/systemd/user/steamos-powerbuttond.service
 	install -D -m 644 steamos-power-button.rules $(DESTDIR)/usr/lib/udev/rules.d/80-steamos-power-button.rules
 	install -D -m 644 steamos-power-button.hwdb $(DESTDIR)/usr/lib/udev/hwdb.d/80-steamos-power-button.hwdb
--- powerbuttond.c
+++ powerbuttond.c
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: BSD-2-Clause
 //
-// Copyright (c) 2023 Valve Software
+// Copyright (c) 2023-2025 Valve Software
 // Maintainer: Vicki Pfau <vi@endrift.com>
 #include <errno.h>
 #include <fcntl.h>
 #include <libevdev/libevdev.h>
+#include <libudev.h>
 #include <limits.h>
 #include <signal.h>
 #include <spawn.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -19,7 +21,7 @@ extern char** environ;
 void signal_handler(int) {
 }
 
-struct libevdev* find_dev(const char* path) {
+struct libevdev* open_dev(const char* path) {
 	int fd = open(path, O_RDONLY);
 	if (fd < 0) {
 		return NULL;
@@ -33,6 +35,62 @@ struct libevdev* find_dev(const char* path) {
 		libevdev_free(dev);
 		return NULL;
 	}
+	return dev;
+}
+
+struct libevdev* find_dev(void) {
+	struct udev* udev = udev_new();
+	char* path = NULL;
+	if (!udev) {
+		return NULL;
+	}
+	struct udev_enumerate* uenum = udev_enumerate_new(udev);
+	if (!uenum) {
+		goto out;
+	}
+
+	if (udev_enumerate_add_match_subsystem(uenum, "input") < 0) {
+		goto out;
+	}
+
+	if (udev_enumerate_add_match_sysname(uenum, "event*") < 0) {
+		goto out;
+	}
+
+	if (udev_enumerate_add_match_property(uenum, "STEAMOS_POWER_BUTTON", "1") < 0) {
+		goto out;
+	}
+
+	if (udev_enumerate_scan_devices(uenum) < 0) {
+		goto out;
+	}
+
+	 struct udev_list_entry* devices = udev_enumerate_get_list_entry(uenum);
+
+	 for (; devices && !path; devices = udev_list_entry_get_next(devices)) {
+		const char* syspath = udev_list_entry_get_name(devices);
+		struct udev_device* dev = udev_device_new_from_syspath(udev, syspath);
+		if (!dev) {
+			continue;
+		}
+		const char* devpath = udev_device_get_devnode(dev);
+		if (devpath) {
+			path = strdup(devpath);
+		}
+		udev_device_unref(dev);
+	 }
+
+out:
+	if (uenum) {
+		udev_enumerate_unref(uenum);
+	}
+	udev_unref(udev);
+
+	if (path == NULL) {
+		return NULL;
+	}
+	struct libevdev* dev = open_dev(path);
+	free(path);
 	return dev;
 }
 
@@ -60,10 +118,6 @@ void do_press(const char* type) {
 }
 
 int main(int argc, char* argv[]) {
-	const char* path = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
-	if (argc >= 2) {
-		path = argv[1];
-	}
 	struct sigaction sa = {
 		.sa_handler = signal_handler,
 		.sa_flags = SA_NOCLDSTOP,
@@ -71,9 +125,14 @@ int main(int argc, char* argv[]) {
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGALRM, &sa, NULL);
 
-	struct libevdev* dev = find_dev(path);
+	struct libevdev* dev;
+	if (argc >= 2) {
+		dev = open_dev(argv[1]);
+	} else {
+		dev = find_dev();
+	}
 	if (!dev) {
-		return 1;
+		return 0;
 	}
 
 	bool press_active = false;
